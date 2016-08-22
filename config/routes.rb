c44091d0dc1b8136d28affdf312e1f0d547ba155
gaa7par <gaa7par@gmail.com>
2016-08-22 13:58:10 +0200

    Update routes.rb and configure devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root 'home#index'
+
+  namespace :user do
+    resources :languages do
+      resources :flashcards
+    end
+  end
+
+  resources :users
 end
