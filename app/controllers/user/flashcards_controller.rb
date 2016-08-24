b7c892f6a2c4dbc3fc76b53ebceaf815d554331f
gaa7par <gaa7par@gmail.com>
2016-08-24 13:11:02 +0200

    Write Users, Languages and Flashcards controllers

--- app/controllers/user/flashcards_controller.rb
+++ app/controllers/user/flashcards_controller.rb
@@ -1,2 +1,52 @@
 class User::FlashcardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @flashcards = Flashcard.all
+  end
+
+  def show
+    @flashcard = Flashcard.find(params[:id])
+  end
+
+  def new
+    @flashcard = Flashcard.new
+  end
+
+  def edit
+    @flashcard = Flashcard.find(params[:id])
+  end
+
+  def create
+    @flashcard = Flashcard.new(flashcard_params)
+
+    if @flashcard.save
+      redirect_to [:user, @flashcard]
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @flashcard = Flashcard.find(params[:id])
+
+    if @flashcard.update(flashcard_params)
+      redirect_to [:user, @flashcard]
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @flashcard = Flashcard.find(params[:id])
+    @flashcard.destroy
+
+    redirect_to [:user, @flashcard]
+  end
+
+  private
+
+  def flashcard_params
+    params.require(:flashcard).permit(:front, :back)
+  end
 end
--- app/controllers/user/languages_controller.rb
+++ app/controllers/user/languages_controller.rb
@@ -1,2 +1,52 @@
 class User::LanguagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @languages = Language.all
+  end
+
+  def show
+    @language = Language.find(params[:id])
+  end
+
+  def new
+    @language = Language.new
+  end
+
+  def edit
+    @language = Language.find(params[:id])
+  end
+
+  def create
+    @language = Language.new(language_params)
+
+    if @language.save
+      redirect_to [:user, @language]
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @language = Language.find(params[:id])
+
+    if @language.update(language_params)
+      redirect_to [:user, @language]
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @language = Language.find(params[:id])
+    @language.destroy
+
+    redirect_to [:user, @language]
+  end
+
+  private
+
+  def language_params
+    params.require(:language).permit(:name)
+  end
 end
