494d81fc6611b863a4a07e2cc2350e3e1d42b777
gaa7par <gaa7par@gmail.com>
2016-09-06 15:49:53 +0200

    Add language policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def not_authorized
+    redirect_to root_path, notice: "Some text"
+  end
 end
