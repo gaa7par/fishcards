61b3a85c01394ad203cc88032451311dd2a0a09d
gaa7par <gaa7par@gmail.com>
2016-09-12 15:48:04 +0200

    Add quiz method to Language model

--- app/models/language.rb
+++ app/models/language.rb
@@ -3,4 +3,8 @@ class Language < ApplicationRecord
   has_many :flashcards, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :user_id, presence: true
+
+  def quiz(number = 5)
+    flashcards.all.shuffle[0..number - 1]
+  end
 end
