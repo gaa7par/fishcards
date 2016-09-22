b2507754507664ed01567203f57ee6eb6134bb3c
gaa7par <gaa7par@gmail.com>
2016-09-22 14:14:49 +0200

    Move js to view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,3 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-
-$(document).ready(function() {
-  $("#switch").click(function() {
-    $("#list").toggle();
-  });
-});
