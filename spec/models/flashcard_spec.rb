8b7c4f2448c1ee61d36b8a593bf770edbd497253
gaa7par <gaa7par@gmail.com>
2016-10-04 15:42:06 +0200

    Tests the_same? method

--- spec/models/flashcard_spec.rb
+++ spec/models/flashcard_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Flashcard, type: :model do
   it 'has valid factory' do
     expect(build(:flashcard)).to be_valid
   end
+
+  describe '#the_same?' do
+    let(:flashcard) { create(:flashcard, front: 'el chico', back: 'the boy') }
+
+    context 'correct answer' do
+      let(:params) { { back: 'the boy' } }
+
+      it { expect(flashcard.the_same?(params)).to eq true }
+    end
+
+    context 'incorrect answer' do
+      let(:params) { { back: 'the girl' } }
+
+      it { expect(flashcard.the_same?(params)).to eq false }
+    end
+  end
 end
