0ebd75d901021050791366823e2876bec6f5a603
gaa7par <gaa7par@gmail.com>
2016-10-07 10:32:55 +0200

    Add sessions_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   include Pundit
+
   protect_from_forgery with: :exception
 
+  before_filter :banned?
+
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def banned?
+    if current_user.present? && current_user.banned?
+      sign_out current_user
+      flash[:error] = "Account banned!"
+      root_path
+    end
+  end
 
   def not_authorized
     redirect_to root_path, notice: 'Permission denied!'
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < Devise::SessionsController
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.banned?
+      sign_out resource
+      flash[:error] = "Account banned!"
+      root_path
+    else
+      super
+    end
+  end
+end
