371206e79b48c6ea008abb6079ae7f3d6fcc2b80
gaa7par <gaa7par@gmail.com>
2016-10-13 13:00:28 +0200

    Test Users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,42 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create(:user) }
+  before { sign_in user }
+
+  describe '#index' do
+    let(:call_request) { get :index }
+
+    context 'after request' do
+      before { call_request }
+
+      it { should render_template 'index' }
+    end
+  end
+
+  describe '#show' do
+    let(:call_request) { get :show, params: { id: user.id } }
+
+    context 'after request' do
+      before { call_request }
+
+      it { should render_template 'show' }
+      it { expect(assigns(:user)).to eq user }
+    end
+  end
+
+  describe '#edit' do
+    let(:call_request) { get :edit, params: { id: user.id } }
+
+    context 'after request' do
+      before { call_request }
+
+      it { should render_template 'edit' }
+      it { expect(assigns(:user)).to eq user }
+    end
+  end
+
   describe '#ban' do
-    let(:user) { create(:user, banned: false) }
     let(:call_request) { patch :ban, params: { id: user.id } }
 
     context 'after request' do
@@ -12,11 +46,11 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe '#unban' do
-    let(:user) { create(:user, banned: true) }
-    let(:call_request) { patch :unban, params: { id: user.id } }
+    let(:call_request) { patch :unban, params: { id: banned_user.id } }
+    let!(:banned_user) { create(:user, banned: true) }
 
     context 'after request' do
-      it { expect { call_request }.to change { user.reload.banned }.from(true).to(false) }
+      it { expect { call_request }.to change { banned_user.reload.banned }.from(true).to(false) }
     end
   end
 end
