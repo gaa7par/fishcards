3f730f5919301459dac44d88c24fa0e9324a3278
gaa7par <gaa7par@gmail.com>
2016-10-17 14:13:16 +0200

    Test Language#random_flashcard method

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Language, type: :model do
   it 'has valid factory' do
     expect(build(:language)).to be_valid
   end
+
+  describe '#random_flashcard' do
+    let(:language) { create(:language) }
+    let(:flashcard1) { create(:flashcard, id: 1, language_id: language.id) }
+    let(:flashcard2) { create(:flashcard, id: 2, language_id: language.id) }
+    let(:flashcard3) { create(:flashcard, id: 3, language_id: language.id) }
+
+    context 'should return random flashcard' do
+      let(:flashcards) { [flashcard1, flashcard2, flashcard3] }
+
+      it { expect(flashcards).to include language.random_flashcard }
+    end
+  end
 end
