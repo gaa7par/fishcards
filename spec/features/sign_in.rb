e5ee0829152653d26d69cba77a6f8d2c26d5fbbd
gaa7par <gaa7par@gmail.com>
2016-10-20 09:35:03 +0200

    Test incorrect sign in

--- spec/features/sign_in.rb
+++ spec/features/sign_in.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Sign in' do
   let!(:user) { create(:user, email: 'alpha@example.com', password: 'secret') }
 
-  scenario 'user logs in' do
+  scenario 'user logs in correctly' do
     visit new_user_session_path
 
     fill_in 'user_email', with:  'alpha@example.com'
@@ -12,5 +12,16 @@ RSpec.feature 'Sign in' do
 
     expect(page).to have_content 'Signed in successfully.'
     expect(page).to have_current_path(root_path)
+  end
+
+  scenario 'user logs in incorrectly' do
+    visit new_user_session_path
+
+    fill_in 'user_email', with: nil
+    fill_in 'user_password', with: nil
+    click_button('Log in')
+
+    expect(page).to have_content 'Invalid Email or password.'
+    expect(page).to have_current_path(new_user_session_path)
   end
 end
